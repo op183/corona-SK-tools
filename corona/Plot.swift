4b145eb168cbab654196c25d9f41ac4550bd1b1a
Ivo Vacek <ivo.vacek@gmail.com>
2020-04-07 16:20:50 +0200

    .ref

--- corona/Plot.swift
+++ corona/Plot.swift
@@ -183,9 +183,3 @@ struct PlotInfectionRate: View {
         }
     }
 }
-
-struct Plot_Previews: PreviewProvider {
-    static var previews: some View {
-        /*@START_MENU_TOKEN@*/Text("Hello, World!")/*@END_MENU_TOKEN@*/
-    }
-}
