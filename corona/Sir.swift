8b14d54d4ab606d1fbd93f9b5a11ea5685369fc4
Ivo Vacek <ivo.vacek@gmail.com>
2020-04-26 16:34:38 +0200

    prediction on faze one (first faze of the relax)

--- corona/Sir.swift
+++ corona/Sir.swift
@@ -98,12 +98,16 @@ class SIRModel: ObservableObject {
     @Published var current = true
     // apply parameters (lambda, lambdaISP, kappa) BEFORE day
     //
-    var parameters: [Parameters] = [Parameters(day: 6, lambda: 0.753, lambdaISP: 0.67, kappa: 0.0450), // fix (prve opatrenia)
-                                    Parameters(day: 19, lambda: 0.753, lambdaISP: 0.61, kappa: 0.0450), // fix (rúška)
-                                    Parameters(day: 32, lambda: 0.608, lambdaISP: 0.524, kappa: 0.0380), // fix 03/04 karantena
+    var parameters: [Parameters] = [Parameters(day: 6, lambda: 0.753, lambdaISP: 0.67, kappa: 0.0450), // fix (prve opatrenia) 0 ... 6
+                                    Parameters(day: 19, lambda: 0.753, lambdaISP: 0.61, kappa: 0.0450), // fix (rúška)  6 ... 19
+                                    Parameters(day: 32, lambda: 0.608, lambdaISP: 0.524, kappa: 0.0380), // fix 06/04 karantena 19 ... 32
                                     
-                                    Parameters(day: 47, lambda: 0.578, lambdaISP: 0.468, kappa: 0.0477), // tmp ??? fix, uvolnenie
-                                    
+                                    Parameters(day: 47, lambda: 0.578, lambdaISP: 0.468, kappa: 0.0477), // tmp ??? fix, uvolnenie 32 ... 47
+        // pd dni 47 sa spustila 1. relaxacná fáza, odhad parametrov 0.667, 0.544, 0.045 založený na A(i)/A(i-1) cca 1.05 (5% denný nárast
+        // bude možné spresniť za 14 dní
+        //
+        Parameters(day: 63, lambda: 0.667, lambdaISP: 0.544, kappa: 0.045),
+        //
     ]
     
     var fixed: Int? {
